6dbdfeab629321a45d90944d3a41a1d504403905
Gowthamr221 <gowthamr221@gmail.com>
2022-06-24 00:33:22 +0530

    added gallery section

--- src/MyRouter/AboutMe/AboutMe.jsx
+++ src/MyRouter/AboutMe/AboutMe.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Footer from '../../components/Footer/Footer'
 import NavBar from '../../components/NavBar/NavBar'
 import StatsDisplay from '../../components/StatsDisplay/StatsDisplay'
-
+import Gallery from "../../components/Gallery/Gallery"
 function AboutMe() {
   return (
     <div>
         <NavBar/>
         <StatsDisplay/>
-        
+        <Gallery/>
         <Footer/>
     </div>
   )
