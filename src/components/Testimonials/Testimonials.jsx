f16c2e1320898d4919d70b582333df36aaa9a75e
KODURU SIVA GOWTHAM REDDY <33353028+Gowthamr221@users.noreply.github.com>
2022-11-08 22:42:41 +0530

    Update Testimonials.jsx

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -14,7 +14,7 @@ function Testimonials() {
           </svg>
           <p class="leading-relaxed mb-6">Gowtham has very keen sense of good user interface design and how to create scalable userinterface as well as backend development.He is a good team player in hackathons and team projects.</p>
           <a class="inline-flex items-center">
-//             <img alt="testimonial" src="https://media-exp1.licdn.com/dms/image/C4D03AQG8U1johvZf_g/profile-displayphoto-shrink_800_800/0/1617464475432?e=1661990400&v=beta&t=I9oWWdygXtsu33oExSQ5R6mzHv1_PV5LtczNGOUvaRA" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"></img>
+           <img alt="testimonial" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"></img>
             <span class="flex-grow flex flex-col pl-4">
               <span class="title-font font-medium text-white">Jayesh jayanandan</span>
               <span class="text-gray-500 text-sm">Full Stack Web Developer @AB_inBev</span>
@@ -29,7 +29,7 @@ function Testimonials() {
           </svg>
           <p class="leading-relaxed mb-6">Gowtham is self starter, good at comparing resources and choosing whats right fit for the scenario.He actively took part in  Product prototyping during his internship period.He is a valubale asset for any company   </p>
           <a class="inline-flex items-center">
-//             <img alt="testimonial" src="https://media-exp1.licdn.com/dms/image/C4D0BAQGGZ4iDhWOBBw/company-logo_100_100/0/1638896167163?e=1664409600&v=beta&t=OgI3pNDMFzWDHSEIYtzfpMfWwII5S6v3RLEhw31Ydpw" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"></img>
+            <img alt="testimonial" src="https://media-exp1.licdn.com/dms/image/D4D03AQEoZM2Osd3fAQ/profile-displayphoto-shrink_800_800/0/1666027100245?e=1673481600&v=beta&t=G5vJJDGwxLVxYbSMKQuaFR9A__4WpQUjBzs12DpJsX8" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"></img>
             <span class="flex-grow flex flex-col pl-4">
               <span class="title-font font-medium text-white">Ram K</span>
               <span class="text-gray-500 text-sm">@Creamzo</span>
